853564fc8db5f778d18b52f39d0d7afa4b0a5c2b
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-01-23 16:32:17 -0300

    aditando o componente do modal

--- src/components/layout/AddFAB/AddFAB.jsx
+++ src/components/layout/AddFAB/AddFAB.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 
 import './AddFAB.css';
 
-const AddFAB = () => {
-    return (
-        <button type="button" className="AddFAB">
-            <strong>+</strong>
-        </button>
-    )
+
+class AddFAB extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            show: false
+        }
+
+        this.showModal = this.showModal.bind(this);
+    }
+
+    showModal() {
+        this.setState({ show: true });
+        console.log(this.state.show)
+    }
+
+    hideModal() {
+        this.setState({ show: false });
+    }
+
+    render() {
+        return (
+            <button type="button" onClick={this.showModal()} className="AddFAB">
+                <strong>+</strong>
+            </button>
+
+        )
+    }
 }
 
 export default AddFAB;
