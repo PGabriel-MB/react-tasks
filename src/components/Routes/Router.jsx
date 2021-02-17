9680684dd4705c0e6b0478530d13d83ca1a01593
pablo.barbosa <pablo.barbosa@anovasistemas.com.br>
2021-02-17 16:41:03 -0300

    criando rotas da aplicaçao

---
+++ src/components/Routes/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import App from '../../App';
+import Login from "../../pages/Login";
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/dashboard" component={App} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return <h1>Eu sou uma página de Login!</h1>
+    }
+}
+
+export default Login;
