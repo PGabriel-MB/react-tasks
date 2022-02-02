fa46dab760203025281357546852a066e212954a
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-02-01 22:08:16 -0300

    tela de cadastro

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CheckCircle } from 'react-feather';
+import { Link } from "react-router-dom";
 
 import {
     Container,
@@ -24,8 +25,60 @@ export const Register = () => {
                     id="name"
                     name="name"
                     placeholder="Nome"
-
                 />
+                <Input
+                    id="email"
+                    name="email"
+                    placeholder="E-mail"
+                    type="email"
+                    inputMode="email"
+                />
+                <Input
+                    id="password1"
+                    name="password1"
+                    placeholder="Password"
+                    type="password1"
+                />
+                <Input
+                    id="password2"
+                    name="password2"
+                    placeholder="Confirm password"
+                    type="password2"
+                />
+                <br />
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    padding: '1em 1em'
+                }}>
+                    <Button
+                        id="loginButton"
+                        type="button"
+                        style={{
+                            border: 'none',
+                            backgroundColor: '#47C2B1',
+                            color: 'white',
+                        }}
+                    >Cadastrar</Button>
+                    <Link
+                        to="/"
+                        id="goToLogin"
+                        className="buttonStyle"
+                        type="button"
+                        style={{
+                            border: '2px solid #47C2B1',
+                            color: '#47C2B1',
+                            backgroundColor: '#ffffff',
+                            minWidth: '8rem',
+                            fontWeight: '900',
+                            borderRadius: '7px',
+                            padding: '.5rem .7rem',
+                            fontSize: '1em',
+                            textDecoration: 'none'
+                        }}
+                    > Login </Link>
+                </div>
             </Form>
         </Container>
     )
