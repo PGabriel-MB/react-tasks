c8a5e5caa9eb72f640c59f1a6ce312ad67025c52
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-02-24 21:51:36 -0300

    trabalhando em animaçao de login

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import { RefreshCcw } from "react-feather";
+import styled from "styled-components";
+
+const Container = styled.div``
+
+export const Loading = () => {
+    return (
+        <Container>
+            <RefreshCcw id="refreshIcon" />
+        </Container>
+    )
+}
