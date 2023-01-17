5c548b56e4c8da8f67ba0729a4721695e81f3946
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-01-17 10:02:17 -0300

    alinhamento de painel - tela delogin

--- src/components/ui/Container/styles.ts
+++ src/components/ui/Container/styles.ts
@@ -4,9 +4,12 @@ export const Flex = styled.div`
 	display: flex;
 	justify-content: center;
 	align-content: center;
+	width: 100%;
+	height: 100%;
 `;
 
 export const Box = styled.div`
 	width: 100%;
 	max-width: 550px;
+	margin: 10em 0;
 `;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -7,25 +7,23 @@ import * as S from "./style";
 export const Auth: React.FC = () => {
   return (
 		<Container>
-			<S.VeritcalSpacing>
-				<S.Card>
-					<S.Input placeholder="Email"/>
-					<S.Input placeholder="Password"/>
-					<S.ButtonRow>
-						<S.Button
-							style={{
-								color: "#ffffff",
-								backgroundColor: "var(--green-dark)"
-							}}
-						>
-							Login
-						</S.Button>
-						<S.Button style={{color: "var(--green-dark)"}}>
-							Cadastro
-						</S.Button>
-					</S.ButtonRow>
-				</S.Card>
-			</S.VeritcalSpacing>
+			<S.Card>
+				<S.Input placeholder="Email"/>
+				<S.Input placeholder="Password"/>
+				<S.ButtonRow>
+					<S.Button
+						style={{
+							color: "#ffffff",
+							backgroundColor: "var(--green-dark)"
+						}}
+					>
+						Login
+					</S.Button>
+					<S.Button style={{color: "var(--green-dark)"}}>
+						Cadastro
+					</S.Button>
+				</S.ButtonRow>
+			</S.Card>
 		</Container>
 	);
 }
